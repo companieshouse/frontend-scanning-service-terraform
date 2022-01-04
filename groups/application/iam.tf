acddf99a67549f8865c019bc819ef1bff2b4c68e
MartyT <martytaggart@gmail.com>
2022-01-04 14:26:57 +0000

    change to increment version

--- groups/application/iam.tf
+++ groups/application/iam.tf
@@ -22,6 +22,7 @@ module "fes_app_profile" {
     local.ssm_kms_key_id
   ]
   s3_buckets_write = [local.session_manager_bucket_name]
+  
   custom_statements = [
     {
       sid    = "AllowAccessToReleaseBucket",
@@ -38,4 +39,5 @@ module "fes_app_profile" {
       ]
     }
   ]
+  
 }
